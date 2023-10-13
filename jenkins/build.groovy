6525621058994336064e6a89f71c31ac4f6ed2fd
AD-Chauhan <65841810+AD-Chauhan@users.noreply.github.com>
2023-10-13 12:44:47 +0530

    Update build.groovy

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -24,7 +24,7 @@ pipeline{
 	
 	 stage("Deploy Image->K8s"){
          steps{
-      bat 'kubectl apply -f docker-k8s.yaml --context minikube'  
+      bat 'kubectl apply -f docker-k8s.yaml'  
     }
     }
     }
